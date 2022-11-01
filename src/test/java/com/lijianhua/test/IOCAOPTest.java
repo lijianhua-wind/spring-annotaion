b625f30750001ac90af5a1b24e59459302e837ba
午夜.bicycle <1976288368@qq.com>
2022-11-01 23:43:28 +0800

    添加

--- src/test/java/com/lijianhua/test/IOCAOPTest.java
+++ src/test/java/com/lijianhua/test/IOCAOPTest.java
@@ -3,7 +3,9 @@ package com.lijianhua.test;
 import com.lijianhua.aop.MathCalculator;
 import com.lijianhua.config.MainConfigOfAOP;
 import com.lijianhua.config.MainConfigOfLifeCycle;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -21,10 +23,12 @@ public class IOCAOPTest {
         System.out.println("容器创建完成");
         MathCalculator calculator
                 = applicationContext.getBean(MathCalculator.class);
-        System.out.println(calculator.div(1, 1));
+        int result = calculator.div(1, 1);
+        System.out.println(result);
         //关闭容器
         applicationContext.close();
     }
+    @AfterThrowing
 
     private void printBeans(ApplicationContext applicationContext) {
         String[] names = applicationContext.getBeanDefinitionNames();
